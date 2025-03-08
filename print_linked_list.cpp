25fe10ad609e67ea35926455ea935022aa9c3f6d
Maheer Tajwar <maheertajwar@gmail.com>
2025-03-08 19:45:10 +0600

    first commit

---
+++ print_linked_list.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* next;
+    Node(int val) {
+        this->val=val;
+        this->next=NULL;
+        }
+};
+int main()
+{
+    Node* head=new Node(10);
+    Node* a=new Node(20);
+    Node* b=new Node(30);
+    Node* c=new Node(40);
+    Node* d=new Node(50);
+    Node* e=new Node(60);
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    d->next=e;
+
+    // cout<<head->val<<endl<<head->next->val<<endl<<head->next->next->val<<endl<<head->next->next->next->val;
+
+    // while(head!=NULL){
+    //     cout<<head->val<<endl;
+    //     head=head->next;
+    // }
+
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<endl;
+        temp=temp->next;
+    }
+}
